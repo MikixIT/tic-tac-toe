c47b1f4d98d03e5dce07423613d38d4f8bb0d302
MikixIT <michael.46@live.it>
2023-10-17 01:07:28 +0200

    working on tictactoe component and make and example for improve my usestate skills with react

--- tic-tac-toe/src/Components/TicTacToe/TicTacToe.jsx
+++ tic-tac-toe/src/Components/TicTacToe/TicTacToe.jsx
@@ -1,11 +1,23 @@
-// import React from "react"
-import './TicTacToe.css'
+import React, { useState } from 'react'
+import './TicTacToe.css' 
+import circle_img from '../Assets/circle.png'
+import cross_img from '../Assets/cross.png'
+
 
 const TicTacToe = () => {
+    let [a, setA] = useState(0);
+    const aPlus = () => {
+        setA(a++)
+    }
+
     return(
-        <>
-        <h1>Tic Tac Toe component</h1>
-        </>
+        <div className='container'>
+            <h1 className="title">Tic Tac Toe Game in ⚛️ <span>React</span></h1>
+            <div className="board">
+
+            </div>
+            <button onClick={aPlus} className="reset_game"> Reset {a}</button>
+        </div>
     )
 }
 
