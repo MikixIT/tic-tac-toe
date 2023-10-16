e42a6cbcc4879681418fac44817a7bca56817e0c
MikixIT <michael.46@live.it>
2023-10-16 19:03:15 +0200

    create component tictactoe

---
+++ tic-tac-toe/src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,12 @@
+// import React from "react"
+import './TicTacToe.css'
+
+const TicTacToe = () => {
+    return(
+        <>
+        <h1>Tic Tac Toe component</h1>
+        </>
+    )
+}
+
+export default TicTacToe
